0fb970d029c1c77658e2259ec2221f23b66e5252
rhchaves <rodolfohenriquechaves@gmail.com>
2022-04-21 15:27:40 -0300

    criado componente CategoriesComponent e classes no CSS

---
+++ src/components/CategoriesComponent.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="btnAmber q-pa-xs row justify-center">
+      <q-btn
+        v-for="category in listCategories"
+        :key="category.name"
+        class="btnAmber categories"
+        :label="category.label"
+        @click="categories(category.name)"
+      />
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CategoriesComponent',
+
+  data() {
+    return {
+      listCategories: [
+        { name: 'construcao', label: 'Construção' },
+        { name: 'eletrica', label: 'Elétrica' },
+        { name: 'hidraulica', label: 'Hidráulica' },
+        { name: 'ferragens', label: 'Ferragens' },
+        { name: 'tintas', label: 'Tintas' },
+        { name: 'ferramentas', label: 'Ferramentas' },
+      ],
+    };
+  },
+
+  methods: {
+    categories(item) {
+      console.log('Clicou na categoria: ', item);
+    },
+  },
+};
+</script>
