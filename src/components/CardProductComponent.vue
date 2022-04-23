dea5b2d82419cb1ef5b5e2bc8414b2a49f98b174
rhchaves <rodolfohenriquechaves@gmail.com>
2022-04-23 08:49:49 -0300

    inserido props para controle do componente e botão para compra direta

--- src/components/CardProductComponent.vue
+++ src/components/CardProductComponent.vue
@@ -1,26 +1,32 @@
 <template>
   <div class="q-ma-md">
     <!-- card  -->
-    <q-card class="my-card" v-for="item in listItems" :key="item.id">
+    <q-card class="my-card">
       <q-card-section horizontal>
         <!-- carrega a imagem do card -->
         <q-img
           class="col q-ma-sm"
-          :src="item.image"
-          :alt="item.teste"
+          :src="imageItem"
+          :alt="altImageItem"
         />
         <!-- exibir botões -->
         <q-card-actions vertical class="justify-aroun">
-          <q-btn class="btnAmber" icon="add_shopping_cart" />
-          <q-btn class="btnAmber" icon="favorite_border" />
-          <q-btn class="btnAmber" icon="share" />
+          <q-btn class="btnAmber" icon="add_shopping_cart" @click="addCartItem(idItem)"/>
+          <q-btn class="btnAmber" icon="favorite_border" @click="addFavoriteItem(idItem)"/>
+          <q-btn class="btnAmber" icon="share" @click="shareItem(idItem)"/>
         </q-card-actions>
 
       </q-card-section>
-      <!-- exibir texto -->
-      <q-card-section>
-        <span><b>{{ item.label }}</b></span><br>
-        <span><b>R${{ item.price }}</b></span>
+      <!-- exibir texto e botão comprar -->
+      <q-card-section class="row">
+        <div class="col col-md-7">
+          <span><b>{{ labelItem }}</b></span><br>
+          <span><b>R${{ priceItem }}</b></span>
+        </div>
+        <q-space />
+        <div class="col col-md-5">
+          <q-btn class="btnAmber" rounded @click="buyItem(idItem)">Comprar</q-btn>
+        </div>
       </q-card-section>
 
     </q-card>
@@ -32,14 +38,66 @@
 export default {
   name: 'CardProductComponent',
 
+  props: {
+    idItem: {
+      type: Number,
+      required: true,
+    },
+    nameItem: {
+      type: String,
+      required: false,
+    },
+    labelItem: {
+      type: String,
+      required: true,
+    },
+    priceItem: {
+      type: String,
+      required: true,
+    },
+    imageItem: {
+      type: String,
+      required: false,
+    },
+    altImageItem: {
+      type: String,
+      required: false,
+      default: 'Imagem do produto',
+    },
+  },
+
   data() {
     return {
-      listItems: [
-        {
-          id: 1, name: 'fita', label: 'Fital Isolante', price: '19,90', image: '/img/fita_isolante.jpg', teste: 'Fita isolante Scotch 33+',
-        },
-      ],
+      // Lista de itens para teste
+      // listItems: [
+      //   {
+      //     id: 1,
+      //     name: 'fita',
+      //     label: 'Fita Isolante',
+      //     price: '19,90',
+      //     image: 'img/fita_isolante.jpg',
+      //     altImage: 'Fita isolante Scotch 33+',
+      //   },
+      // ],
     };
+  },
+
+  methods: {
+    // Funções para enviar ao elemento 'pai' o sinal de que cada botão foi clicado
+    addCartItem(idItem) {
+      this.$emit('addCartItemEmit', idItem);
+    },
+
+    addFavoriteItem(idItem) {
+      this.$emit('addFavoriteItemEmit', idItem);
+    },
+
+    shareItem(idItem) {
+      this.$emit('shareItemEmit', idItem);
+    },
+    buyItem(idItem) {
+      this.$emit('buyItemEmit', idItem);
+    },
   },
 };
 </script>
