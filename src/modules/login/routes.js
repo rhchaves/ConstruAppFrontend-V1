bbb119374458d5852cb1c6f92a05d6daf91bee0f
rhchaves <rodolfohenriquechaves@gmail.com>
2022-04-27 10:03:05 -0300

    criação do modulo de login

---
+++ src/modules/login/routes.js
@@ -0,0 +1,11 @@
+import LoginPage from './pages/LoginPage.vue';
+
+const routes = [
+  {
+    path: 'login',
+    name: 'login',
+    componet: LoginPage,
+  },
+];
+
+export default routes;
--- src/router/routes.js
+++ src/router/routes.js
@@ -3,18 +3,21 @@ import MainPageRoute from '../modules/main-page/routes';
 import ProductPageRoute from '../modules/product-page/routes';
 import RegisterProductRoute from '../modules/register-product/routes';
 import RegisterUserRoute from '../modules/register-user/routes';
+import LoginPageRoute from '../modules/login/routes';
 
 const routes = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('src/modules/main-page/pages/MainPage.vue') },
+      { path: '', component: () => import('src/modules/login/pages/LoginPage.vue') },
+      // { path: '', component: () => import('src/modules/main-page/pages/MainPage.vue') },
       ...AdminPageRoute,
       ...MainPageRoute,
       ...ProductPageRoute,
       ...RegisterProductRoute,
       ...RegisterUserRoute,
+      ...LoginPageRoute,
     ],
   },
 
