91a1f25184abb94652094fc894a330eb5c8b50ca
rhchaves <rodolfohenriquechaves@gmail.com>
2022-05-02 23:34:30 -0300

    ajuste na lógica da página

--- src/modules/cart-page/components/CartListItem.vue
+++ src/modules/cart-page/components/CartListItem.vue
@@ -7,23 +7,26 @@
         </q-item-section>
 
         <q-item-section>
-          <q-item-label>Nome do item</q-item-label>
-          <q-item-label >Descrição breve do item</q-item-label>
-          <q-item-label >Valor: R$ {{ itemValue }}</q-item-label>
+          <q-item-label v-model="item.name">Nome do item</q-item-label>
+          <q-item-label v-model="item.description">Descrição breve do item</q-item-label>
+          <q-item-label v-model="item.value">Valor: R$ {{ item.value }}</q-item-label>
         </q-item-section>
 
         <q-item-section >
-          <q-btn flat icon="delete" class="" >
-              {{ deleteItem }}
-            </q-btn>
+          <q-btn flat icon="delete" class="" v-model="item.id" @click="deleteItem()"></q-btn>
         </q-item-section>
         <q-item-section class="btnAmber " >
-          <q-btn flat icon="add" class="" ></q-btn>
-          <q-input>{{ itemQuantity }}</q-input>
-          <q-btn flat icon="remove" class="" ></q-btn>
+
+          <q-btn flat icon="add" class="" @click="addQuantity"></q-btn>
+          <q-input
+            v-model="item.quantity"
+            @focusout="checkValue"
+          ></q-input>
+          <q-btn flat icon="remove" class="" @click="removeQuantity"></q-btn>
+
         </q-item-section>
         <q-item-section >
-          <q-item-label >R$ {{ value }}</q-item-label>
+          <q-item-label v-model="item.subtotal">R$ {{ item.subtotal }}</q-item-label>
         </q-item-section>
       </q-item>
     </q-list>
@@ -38,13 +41,36 @@
 export default {
   name: 'CartlistItem',
 
+  // props: {
+
+  //   itemQuantity: {
+  //     type: Number,
+  //     required: true,
+  //     default: 0,
+  //   },
+
+  //   deletedItem: {
+  //     type: String,
+  //   },
+
+  //   options: {
+  //     type: String,
+  //     default: 'Dinheiro, Cartão de Crédito, Cartão de Débito',
+  //   },
+
+  // },
+
   data() {
     return {
       deletedItem: '',
-      itemValue: '1700,00',
-      itemQuantity: '0',
-      value: '1700,00',
-      item: '',
+      item: {
+        id: 1,
+        quantity: 0,
+        value: 17.00,
+        subtotal: 0,
+        name: '',
+        description: '',
+      },
       model: null,
       options: [
         'Dinheiro', 'Cartão de Crédito', 'Cartão de Débito',
@@ -54,7 +80,46 @@ export default {
 
   methods: {
     deleteItem() {
-      console.log('Apagar Item:');
+      console.log('Apagar Item:', this.deletedItem);
+      this.item = [];
+    },
+
+    addQuantity() {
+      this.item.quantity += 1;
+      this.calcSubtotal();
+    },
+
+    removeQuantity() {
+      if (this.item.quantity >= 1) {
+        this.item.quantity -= 1;
+      }
+      this.calcSubtotal();
+    },
+
+    checkNaN(x) {
+      if (this.item.quantity.isNaN(x)) {
+        console.log('É número');
+      }
+    },
+
+    checkValue() {
+      const check = this.item.quantity;
+
+      if (this.item.quantity < 0) {
+        this.item.quantity = 0;
+      }
+
+      if (this.isNaN(check) !== false) {
+        console.log('Não É número');
+      }
+
+      // this.checkNaN(this.item.quantity);
+
+      this.calcSubtotal();
+    },
+
+    calcSubtotal() {
+      this.item.subtotal = this.item.quantity * this.item.value;
     },
   },
 
--- src/modules/cart-page/pages/CartPage.vue
+++ src/modules/cart-page/pages/CartPage.vue
@@ -2,8 +2,6 @@
   <q-page class="q-ma-md">
     <h3>Esta é a página de carrinho</h3>
     <CartlistItem />
-    <CartlistItem />
-    <CartlistItem />
   </q-page>
 </template>
 
@@ -20,8 +18,66 @@ export default {
 
   data() {
     return {
-
+      deletedItem: '',
+      item: {
+        id: 1,
+        quantity: 0,
+        value: 17.00,
+        subtotal: 0,
+        name: '',
+        description: '',
+      },
+      model: null,
+      options: [
+        'Dinheiro', 'Cartão de Crédito', 'Cartão de Débito',
+      ],
     };
   },
+
+  methods: {
+    deleteItem() {
+      console.log('Apagar Item:', this.deletedItem);
+      this.item = [];
+    },
+
+    addQuantity() {
+      this.item.quantity += 1;
+      this.calcSubtotal();
+    },
+
+    removeQuantity() {
+      if (this.item.quantity >= 1) {
+        this.item.quantity -= 1;
+      }
+      this.calcSubtotal();
+    },
+
+    checkNaN(x) {
+      if (this.item.quantity.isNaN(x)) {
+        console.log('É número');
+      }
+    },
+
+    checkValue() {
+      const check = this.item.quantity;
+
+      if (this.item.quantity < 0) {
+        this.item.quantity = 0;
+      }
+
+      if (this.isNaN(check) !== false) {
+        console.log('Não É número');
+      }
+
+      // this.checkNaN(this.item.quantity);
+
+      this.calcSubtotal();
+    },
+
+    calcSubtotal() {
+      this.item.subtotal = this.item.quantity * this.item.value;
+    },
+  },
+
 };
 </script>
