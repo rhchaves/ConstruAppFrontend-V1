8fd2540a761f96ad9d58f55faa03af04207d67e4
rhchaves <rodolfohenriquechaves@gmail.com>
2022-05-10 12:12:11 -0300

    removido a instancia dos componentes address e login

--- src/modules/main-page/pages/MainPage.vue
+++ src/modules/main-page/pages/MainPage.vue
@@ -4,26 +4,20 @@
 
     <h3>Página de Principal</h3>
 
-    <LoginPage />
-    <AddressPage v-if="!logado"/>
     <ContentMainPage />
   </q-page>
 </template>
 
 <script>
 
-import LoginPage from 'src/components/LoginPage.vue';
 import CategoriesComponent from 'src/components/CategoriesComponent.vue';
-import AddressPage from 'src/modules/address-page/pages/AddressPage.vue';
 import ContentMainPage from 'src/modules/main-page/components/ContentMainPage.vue';
 
 export default {
   name: 'MainPage',
 
   components: {
-    LoginPage,
     CategoriesComponent,
-    AddressPage,
     ContentMainPage,
   },
 
