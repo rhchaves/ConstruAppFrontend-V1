ddf3dfcb65d4b221110fbb0b4f30bd76a0a614b4
rhchaves <rodolfohenriquechaves@gmail.com>
2022-05-11 18:27:31 -0300

    alteração especial na development para inserção do store cartPage

--- src/store/index.js
+++ src/store/index.js
@@ -10,6 +10,7 @@ import mainPage from '../modules/main-page/store';
 import productPage from '../modules/product-page/store';
 import registerProduct from '../modules/register-product/store';
 import registerUser from '../modules/register-user/store';
+import cartPage from '../modules/cart-page/store';
 
 /*
  * If not building with SSR mode, you can
@@ -28,6 +29,7 @@ export default function (/* { ssrContext } */) {
       productPage,
       registerProduct,
       registerUser,
+      cartPage,
     },
 
     // enable strict mode (adds overhead!)
