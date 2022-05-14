6984e6f1e554b36aa37c575a4f45b50a9e32ab7b
rhchaves <rodolfohenriquechaves@gmail.com>
2022-05-14 12:24:08 -0300

    modulando a página de login

---
+++ src/modules/login-page/routes.js
@@ -0,0 +1,11 @@
+import LoginPage from 'src/modules/login-page/routes';
+
+const routes = [
+  {
+    path: 'login-page',
+    name: 'login-page',
+    component: LoginPage,
+  },
+];
+
+export default routes;
