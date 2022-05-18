ad39d695eddf07243fc84de5e174a038f3b8c4d7
rhchaves <rodolfohenriquechaves@gmail.com>
2022-05-18 20:54:29 -0300

    Criado página de gerenciar admin

--- src/modules/manage-admin/pages/ManageAdmin.vue
+++ src/modules/manage-admin/pages/ManageAdmin.vue
@@ -3,6 +3,32 @@
 
     <h3>Gerenciar Administradores</h3>
 
+    <div>
+      <q-btn
+        class="btnAmber"
+        type="text"
+        rounded
+      >
+        Cadastrar novo Administrador
+      </q-btn>
+
+      <q-btn
+        class="btnAmber"
+        type="text"
+        rounded
+      >
+        Alterar dados
+      </q-btn>
+
+      <q-btn
+        class="btnAmber"
+        type="text"
+        rounded
+      >
+        Excluir Cadastro
+      </q-btn>
+    </div>
+
   </q-page>
 </template>
 
