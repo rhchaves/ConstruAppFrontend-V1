cd7dc0ed29c7da26e5261bf1334520119c51a681
rhchaves <rodolfohenriquechaves@gmail.com>
2022-06-04 12:02:38 -0300

    Adicinonado tabela de gerenciamento de administradores

--- src/modules/manage-admin/pages/ManageAdmin.vue
+++ src/modules/manage-admin/pages/ManageAdmin.vue
@@ -3,6 +3,13 @@
 
     <h3>Gerenciar Administradores</h3>
 
+    <TableComponent
+      title="Gerenciar Administradores"
+      :columns="columns"
+      :data="data"
+      rowKey="name"
+    />
+
     <RegisterNewAdmin />
 
     <div>
@@ -29,6 +36,15 @@
       >
         Excluir Cadastro
       </q-btn>
+
+      <q-btn
+        class="btnAmber"
+        type="text"
+        rounded
+        to="admin-page"
+      >
+        Voltar
+      </q-btn>
     </div>
 
   </q-page>
@@ -36,6 +52,7 @@
 
 <script>
 
+import TableComponent from 'src/common/TableComponent.vue';
 import RegisterNewAdmin from '../components/RegisterNewAdmin.vue';
 
 export default {
@@ -43,10 +60,63 @@ export default {
 
   components: {
     RegisterNewAdmin,
+    TableComponent,
   },
 
   data() {
     return {
+
+      columns: [
+        {
+          name: 'id',
+          label: 'ID',
+          field: 'id',
+        },
+        {
+          name: 'name',
+          label: 'Nome',
+          field: 'name',
+        },
+        {
+          name: 'cpf',
+          label: 'CPF',
+          field: 'cpf',
+        },
+        {
+          name: 'email',
+          label: 'E-mail',
+          field: 'email',
+        },
+        {
+          name: 'createdIn',
+          label: 'Criado em:',
+          field: 'createdIn',
+        },
+        {
+          name: 'status',
+          label: 'Status',
+          field: 'status',
+        },
+      ],
+
+      data: [
+        {
+          id: 1,
+          name: 'Usuário Teste',
+          cpf: '233.456.456-56',
+          email: 'teste@teste.com',
+          createdIn: '12/01/21',
+          status: 'Ativo',
+        },
+        {
+          id: 2,
+          name: 'Usuário Teste 2',
+          cpf: '233.456.456-56',
+          email: 'teste2@teste.com',
+          createdIn: '24/12/18',
+          status: 'Inativo',
+        },
+      ],
 
     };
   },
