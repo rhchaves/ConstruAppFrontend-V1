c4f77e3b268837e7b667baf503df0e061e7631c0
rhchaves <rodolfohenriquechaves@gmail.com>
2022-06-10 22:48:59 -0300

    Ajuste na formatação da página

--- src/modules/address-page/components/SearchAddress.vue
+++ src/modules/address-page/components/SearchAddress.vue
@@ -5,9 +5,11 @@
 
           <q-card square bordered class="q-pa-lg shadow-1">
             <q-card-section class="q-gutter-md">
-                <q-btn class="" type="text" icon="close" flat round v-close-popup
-              @click="close"></q-btn>
-                <!-- inputs de entrada de dados -->
+              <q-btn class="" type="text" icon="close" flat round v-close-popup
+                @click="close"
+              ></q-btn>
+              <!-- inputs de entrada de dados -->
+              <div class="row">
                 <q-input
                   autofocus
                   outlined
@@ -16,32 +18,38 @@
                   label="Digite o cep"
                   mask="#####-###"
                   unmasked-value
-                  style="max-width: 200px"
+                  style="max-width: 120px"
                   :rules="[val => !! val || 'Campo obrigatório']"
                 />
-                <!-- link para site do correios -->
-                <a href="https://buscacepinter.correios.com.br/app/endereco/index.php"
-                  target="_blank">Não sei meu cep!
-                </a>
+
                 <!-- botão de pesquisar CEP -->
                 <q-btn
-                  class="btnAmber"
+                  class="btnAmber q-ml-md q-mt-sm"
                   label="Preencher com CEP"
                   rounded
                   no-caps
-                  @click="searchForCep()"/>
-
-                <q-input outlined v-model="address.street" type="text" label="Rua"
-                  :rules="[val => !! val || 'Campo obrigatório']"/>
-                <q-input outlined v-model="address.number" type="text" label="Número"
-                  :rules="[val => !! val || 'Campo obrigatório']"/>
-                <q-input outlined v-model="address.complement" type="text" label="Complemento" />
-                <q-input outlined v-model="address.district" type="text" label="Bairro"
-                  :rules="[val => !! val || 'Campo obrigatório']"/>
-                <q-input outlined v-model="address.city" type="text" label="Cidade" readonly
-                  :rules="[val => !! val || 'Campo obrigatório']"/>
-                <q-input outlined v-model="address.state" type="text" label="UF" readonly
-                  :rules="[val => !! val || 'Campo obrigatório']"/>
+                  @click="searchForCep()"
+                  style="height: 36px"
+                />
+              </div>
+              <!-- link para site do correios -->
+              <div class="q-ml-lg">
+                <a href="https://buscacepinter.correios.com.br/app/endereco/index.php"
+                  target="_blank"
+                >Não sei meu cep!
+                </a>
+              </div>
+              <q-input outlined v-model="address.street" type="text" label="Rua"
+                :rules="[val => !! val || 'Campo obrigatório']"/>
+              <q-input outlined v-model="address.number" type="text" label="Número"
+                :rules="[val => !! val || 'Campo obrigatório']"/>
+              <q-input outlined v-model="address.complement" type="text" label="Complemento" />
+              <q-input outlined v-model="address.district" type="text" label="Bairro"
+                :rules="[val => !! val || 'Campo obrigatório']"/>
+              <q-input outlined v-model="address.city" type="text" label="Cidade" readonly
+                :rules="[val => !! val || 'Campo obrigatório']"/>
+              <q-input outlined v-model="address.state" type="text" label="UF" readonly
+                :rules="[val => !! val || 'Campo obrigatório']"/>
             </q-card-section>
             <q-card-actions class="q-px-md">
               <q-btn class="btnAmber" label="Confirmar" rounded
