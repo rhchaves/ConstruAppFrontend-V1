bec086893e4eeda98ad89e5a3881d97d38852733
neferson.oliveira.3 <neferson.oliveira.3@globalhitss.com.br>
2022-09-19 22:23:58 -0300

    Alterado a rota base da api

--- src/boot/HttpClient.js
+++ src/boot/HttpClient.js
@@ -18,7 +18,7 @@ import axios from 'axios';
 // });
 
 const HttpClient = axios.create({
-  baseURL: 'http://localhost:8000/api', // 'http://localhost:8000/api'
+  baseURL: 'http://localhost:8000/api/v1', // 'http://localhost:8000/api'
 });
 
 export default HttpClient;
