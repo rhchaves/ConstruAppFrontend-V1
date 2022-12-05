cda4943530164608daf5dcd8b7966062704963cd
rhchaves <rodolfohenriquechaves@gmail.com>
2022-12-05 10:48:24 -0300

    atualização

--- src/modules/manage-product/store/actions.js
+++ src/modules/manage-product/store/actions.js
@@ -38,11 +38,15 @@ const listAllFilteredProducts = async ({ commit }, payload) => {
   setTimeout(async () => {
     await HttpClient.get(`/categories/${payload.id}/products`).then((response) => {
       const categoryProducts = response.data.categories;
-      categoryProducts.forEach((item) => {
-        item.image = `/image/${item.image}`;
-        // item.image = `/image/originais/${item.name}.jpg`;
+      const products = [];
+      categoryProducts.filter((item) => {
+        if (item.status !== 'inativo' && item.length !== 0) {
+          item.image = `/image/${item.image}`;
+          products.push(item);
+        }
+        return products;
       });
-      commit('LIST_FILTER_PRODUCTS', categoryProducts);
+      commit('LIST_FILTER_PRODUCTS', products);
     }).catch((error) => {
       console.log('Erro na requisição da lista', error);
     }).finally(() => {
@@ -56,12 +60,21 @@ const listTopSellingProducts = async ({ commit, state }) => {
   commit('LOADING', true);
   commit('CLEAR_TOP_SELLING_PRODUCTS');
   const productsList = state.products;
+  let qtd = 0;
+  const products = [];
+  console.log('productsList', productsList);
   productsList.filter((item) => {
-    if (item.id <= 8) {
-      commit('TOP_SELLER_PRODUCTS', item);
+    if (item.status !== 'inativo' && item.length !== 0) {
+      if (qtd <= 8) {
+        item.image = `/image/${item.image}`;
+        products.push(item);
+        qtd += 1;
+      }
     }
-    return item;
+    return products;
   });
+  console.log('products', products);
+  commit('TOP_SELLER_PRODUCTS', products);
   commit('LOADING', false);
 };
 
